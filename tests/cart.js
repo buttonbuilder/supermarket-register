df7f97725b7bd5ae24099789df4cd31eb3da971a
Naveen Bezawada <naveen.bezawada@capitalone.com>
2017-01-10 23:39:04 -0600

    Adding another happy path test

--- tests/cart.js
+++ tests/cart.js
@@ -6,10 +6,13 @@ describe('cart.js', function () {
   describe('Invalid  Body', function (){
     it('should give a 400 with a missing key', function(done){
       request.post('/cart').
-        send({"iteList":"jdhfsjbwkrsns,testing123"}).expect(400 ,done)
+      send({"iteList":"jdhfsjbwkrsns,testing123"}).expect(400 ,done)
     });
     it('should give a 400 with no body', function(done){
       request.post('/cart').expect(400,done)
+    });
+    it('should give a 200 with a right reqyest body', function(done){
+      request.post('/cart').send({"itemList":"A12T-4GH7-QPL9-3N4M"}).expect(200,done)
     });
   });
 });
