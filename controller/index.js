6d38b0f5e8bef3891875301712d432c1116a7209
Naveen Bezawada <naveen.bezawada@capitalone.com>
2017-01-24 22:30:26 -0600

    Adding logic to pass the last test

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var catalog = require('../data/catalog.json')
+var _ = require('underscore')
 
 router.post('/', total);
 
@@ -7,10 +9,35 @@ function total(req, res, next) {
   if (req.body){
     if (req.body.itemList){
       var itemListSplit = req.body.itemList.split(',');
-      res.send(200);
+      var subTotal = 0, price, value;
+      itemListSplit.forEach(function(productCode){
+        value = _.find(catalog.inventory, function (o) {
+          return o.productCode == (productCode.toUpperCase());
+        });
+        if (value) {
+
+          price = parseFloat(value.price);
+          subTotal+=price
+        } else {
+          res.status(400).json({'Reason': 'Bad product Code'})
+        }
+
+      });
+      if (subTotal) {
+
+        var total = subTotal*1.0875
+        total = Math.round(total * 100) / 100
+        res.status(200).send('{"total":"'+JSON.stringify(total)+'"}');
+      } else {
+        res.status(400).json({'Reason': 'Missing item list'})
+      }
+    } else {
+      res.status(400).json({'Reason': 'Invalid Request'})
     }
-    res.status(400).json({'Reason': 'Bad Request missing item list'})
   }
-  res.status(400).json({'Reason': 'Missing body'})
+  else {
+      res.status(400).json({'Reason': 'Missing body'})
+  }
 }
+
 module.exports = router;
